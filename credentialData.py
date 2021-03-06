f5611ef3c4b9e4e9f25e24c02c03f8922c3a68c5
mireille1999 <muhawenimana920@daviscollege.com>
2021-03-06 08:52:35 +0200

    updated credentia data

---
+++ credentialData.py
@@ -0,0 +1,30 @@
+
+class CredentialsData:
+
+    """
+    create new instances
+    """
+
+    credentials = []
+
+    def __init__(self, platform, username, password):
+
+        self.platform = platform
+        self.username = username
+        self.password = password
+
+    def save_credential(self):
+
+        """
+        save credential objects to the credential list
+        """
+        CredentialsData.credentials.append(self)
+
+    @classmethod
+
+    def display_credentials(cls):
+
+        """
+        displays the credentials 
+        """
+        return cls.credentials
